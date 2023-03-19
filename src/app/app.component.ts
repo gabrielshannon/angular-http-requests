a7045df6b2f287212039cba97b5d9203fca1d267
Gabriel Shannon <gabrielshannon96@gmail.com>
2023-03-19 18:31:31 +0000

    error handling

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,23 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { map } from 'rxjs/operators';
+import { map, catchError } from 'rxjs/operators';
 
 import { Post } from './post.model';
 import { PostsService } from './posts.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit {
+export class AppComponent implements OnInit, OnDestroy {
   loadedPosts: Post[] = [];
   isFetching = false;
   error = null;
+  private errorSub: Subscription;
 
   constructor(private http: HttpClient, private postsService: PostsService) {}
 
   ngOnInit() {
+    this.errorSub = this.postsService.error.subscribe(errorMessage=> {
+      this.error = errorMessage
+    });
+
+    this.isFetching = false;
     this.postsService.fetchPosts().subscribe(
       posts => {
         this.loadedPosts = posts;
@@ -43,8 +50,7 @@ export class AppComponent implements OnInit {
         this.isFetching = false;
       },
       error => {
-        this.error = error.message;
-        console.log(error)
+        this.error.next(error.message);
       }
     );
   }
@@ -55,4 +61,9 @@ export class AppComponent implements OnInit {
       this.loadedPosts = [];
     });
   }
+
+  ngOnDestroy(): void {
+    this.errorSub.unsubscribe();
+  }
+
 }
--- src/app/posts.service.ts
+++ src/app/posts.service.ts
@@ -1,11 +1,14 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { map } from 'rxjs/operators';
+import { Subject, throwError } from 'rxjs';
+import { map, catchError } from 'rxjs/operators';
 import { Post } from './post.model';
 
 @Injectable({ providedIn: 'root' })
 export class PostsService {
   constructor(private http: HttpClient) {}
+
+  error = new Subject<string>();
 
   createAndStorePost(title: string, content: string) {
     const postData: Post = { title: title, content: content };
@@ -16,6 +19,8 @@ export class PostsService {
       )
       .subscribe(reponseData => {
         console.log(reponseData);
+      }, error => {
+        this.error.next(error.message);
       });
   }
 
@@ -33,6 +38,11 @@ export class PostsService {
             }
           }
           return postsArray;
+        }),
+        catchError(errorRes => {
+
+          /// send to anylitics
+          return throwError(errorRes);
         })
       );
   }
